e0e02fc35e87f42edd2ddec24dc0e7120ad44390
willetto <trey.willetto@gmail.com>
2022-04-06 16:48:06 -0600

    JS functionality & Responsiveness

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const faqs = document.querySelector('.FAQs');
+const boxes = document.querySelectorAll('.box');
+
+faqs.addEventListener('click', e => {
+  boxes.forEach(box => box.classList.remove('active'));
+  e.target.closest('.box').classList.add('active');
+});
