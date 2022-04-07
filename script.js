3e5c459920ebf86ed5da45b03162a4c0c91dea41
willetto <trey.willetto@gmail.com>
2022-04-07 09:58:35 -0600

    fix hero box positioning & add answer slid-in transition

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const faqs = document.querySelector('.FAQs');
 const boxes = document.querySelectorAll('.box');
 
 faqs.addEventListener('click', e => {
-  boxes.forEach(box => box.classList.remove('active'));
-  e.target.closest('.box').classList.add('active');
+  const targetBox = e.target.closest('.box').classList;
+  if (targetBox.contains('active')) {
+    targetBox.remove('active');
+  } else {
+    boxes.forEach(box => box.classList.remove('active'));
+    targetBox.add('active');
+  }
 });
